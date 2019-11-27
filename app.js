a9bd800b3967030e1da0bef86f0cbd7c8fa08369
jeanluc <mtmjea001@myuct.ac.za>
2019-11-28 00:12:09 +0200

    .

--- app.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    if($(window).height()<582){
-        $(".modal-content").css({overflowY:"auto"});
-    }
-    else{
-        $("#"+appendToId).css({width:"40%"});
-    }
-});
